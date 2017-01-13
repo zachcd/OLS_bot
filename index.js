1d8d83b291136f9a791fbc82027c297e4ffa094a
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2017-01-13 13:24:28 -0500

    some setup for registration

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ bot.on("message", msg => {
   let prefix="-"
 
   if(!msg.content.startsWith(prefix)) return;
+
+  if(msg.author.bot) return;
+
   if (msg.content.startsWith(prefix + "fuck")) {
     msg.channel.sendMessage("Fuck Smegs!");
   }
+  if (msg.content.startsWith(prefix + "register")) {
+
+  }
+});
+
+bot.on("guildMemberAdd", (member) => {
+    console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+    member.guild.defaultChannel.sendMessage(`Welcome to the LoL@Pitt Discord "${member.user.username}"!`);
 });
 
 bot.on('ready', () => {
   console.log('I am ready');
 })
+
+bot.on('error', e => { console.error(e); });
 
 bot.login("MjY5NDg1MDY0ODAxMzUzNzM4.C1qBKQ.q8FGymK1NwvFzKuF1Grx4p4g6As");
