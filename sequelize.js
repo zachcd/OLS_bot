7f3544935e38912f15aa02087f1b36ed6d8c30ef
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2017-01-13 13:50:39 -0500

    database setup

---
+++ sequelize.js
@@ -0,0 +1,25 @@
+var Sequelize = require("sequelize");
+
+var database = new Sequelize('OLS', 'admin', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'database.sqlite'
+});
+
+
+database
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
