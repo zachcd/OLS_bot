fd940f9254408620ee7518be0ec79d32ad86e333
Jacob Hershey <jah215@pitt.edu>
2017-01-13 14:03:17 -0500

    impeach
    Testing that impeach works, will then update with the requested picture
    of John. Token was also reset to the new one.

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ bot.on("message", msg => {
   if (msg.content.startsWith(prefix + "fuck")) {
     msg.channel.sendMessage("Fuck Smegs!");
   }
+  if (msg.content.startsWith(prefix + "impeach")) {
+    msg.channel.sendMessage("This should be a picture!");
+  }
 });
 
 bot.on('ready', () => {
   console.log('I am ready');
 })
-
-bot.login("MjY5NDg1MDY0ODAxMzUzNzM4.C1qBKQ.q8FGymK1NwvFzKuF1Grx4p4g6As");
+bot.login("MjY5NDg1MDY0ODAxMzUzNzM4.C1q0-w.YtX8R_Jio4bGINMrF_gIdGjoj_w");
