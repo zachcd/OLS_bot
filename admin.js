08b02f8e0eee3e53e4373a35ed45777d6a0135df
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2018-01-13 11:43:57 -0500

    not sure was drunk

--- admin.js
+++ admin.js
@@ -1,5 +1,11 @@
+var Main = require('./main.js');
 
+let central = {};
 const admin = (msg) => {
+
+  if(msg.content.startsWith(prefix + "init")) {
+    central = new Main
+  }
   if(msg.content.startsWith(prefix + "players")) {
     adminRole = msg.guild.roles.get("233058087030751232");
     if(msg.member.roles.has(adminRole.id)) {
@@ -10,7 +16,7 @@ const admin = (msg) => {
           var elo = parseInt(player[2]);
           sequelize.Players.create({ FullName: player[0], IGN: player[1], PeakELO : elo, Roles: player[3], GamesPlayed: player[4], Notes:player5}).then(function(playerReturned) {
             playerReturned.update({PeakELO: elo}).then (function(eloupdated) {
-              msg.channel.sendMessage(player[1] + " has been added to the draft." );
+
             })
           });
         })
